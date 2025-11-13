0a85af99aa98d84a51fb5d64c98f2e012c011397
honcharenkom <honcharenkom@posam.sk>
2025-11-13 22:02:18 +0100

    adding camunda dmn engine for DMN model

--- src/main/java/com/example/dynamicform/models/dmn/DecisionTableEngine.java
+++ src/main/java/com/example/dynamicform/models/dmn/DecisionTableEngine.java
@@ -1,26 +1,49 @@
-
 package com.example.dynamicform.models.dmn;
 
 import com.example.dynamicform.domain.EvaluationContext;
 import com.example.dynamicform.domain.FieldDefinition;
+import org.camunda.bpm.dmn.engine.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.InputStream;
+import java.util.*;
 
 public class DecisionTableEngine {
 
+    private final DmnEngine engine;
+    private final DmnDecision decision;
+
+    public DecisionTableEngine() {
+        this.engine = DmnEngineConfiguration
+                .createDefaultDmnEngineConfiguration()
+                .buildEngine();
+
+        InputStream dmn = getClass().getResourceAsStream("/dmn/tenant_A_basic.dmn");
+        this.decision = engine.parseDecision("DetermineFields", dmn);
+    }
+
     public List<FieldDefinition> evaluate(EvaluationContext ctx) {
-        List<DecisionRow> rows = DecisionTables.forTenantAndScheme(ctx.getTenant(), ctx.getCardScheme());
+
+        Map<String, Object> vars = new HashMap<>();
+
+        vars.put("discount",
+                ctx.getDiscounts().isEmpty()
+                        ? ""
+                        : ctx.getDiscounts().iterator().next().name());
+
+        System.out.println("DMN INPUT VARS = " + vars);
+
+        DmnDecisionTableResult result = engine.evaluateDecisionTable(decision, vars);
+
         List<FieldDefinition> out = new ArrayList<>();
-        for (DecisionRow row : rows) {
-            if (row.matches(ctx)) {
-                FieldRegistry.ensureKeysExistOrThrow(row.outputs());
-                for (String key : row.outputs()) {
-                    FieldRegistry.FieldTemplate tmpl = FieldRegistry.lookup(key).orElseThrow();
-                    out.add(tmpl.build(ctx));
-                }
+
+        for (DmnDecisionRuleResult rule : result) {
+            Object val = rule.get("fields");
+            if (val != null) {
+                FieldRegistry.ensureKeysExistOrThrow(List.of(val.toString()));
+                out.add(FieldRegistry.lookup(val.toString()).orElseThrow().build(ctx));
             }
         }
+
         return out;
     }
 }
